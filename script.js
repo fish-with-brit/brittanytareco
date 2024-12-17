3ebd0e34a3974d2723c776db6001a0087e41103e
fish-with-brit <fishwithbrit@gmail.com>
2024-12-17 04:22:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+{/* <script>
+  // Function to show the popup and prevent form submission
+  function showPopup(event) {
+    event.preventDefault(); // Prevent form from refreshing the page
+    const popup = document.getElementById('popup');
+    popup.style.display = 'block';
+  }
+
+  // Function to close the popup
+  function closePopup() {
+    const popup = document.getElementById('popup');
+    popup.style.display = 'none';
+    document.getElementById('replyForm').reset(); // Reset the form fields
+  }
+</script> */}
